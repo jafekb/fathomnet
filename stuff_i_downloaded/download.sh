6b44e84328c73f1595d8f9b5351efe0820d6b010
Ben Jafek <bjafek@nuro.ai>
2025-05-13 13:42:32 -0700

    .gitignore

---
+++ stuff_i_downloaded/download.sh
@@ -0,0 +1,5 @@
+OUT_DIR="/home/bjafek/Nuro/benj_prac/fathomnet/data/train"
+DATASET="/home/bjafek/Nuro/benj_prac/fathomnet/data/train/dataset_train.json"
+python download.py \
+    $DATASET \
+    $OUT_DIR
